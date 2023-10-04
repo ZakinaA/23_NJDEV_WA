7a76ba095bb5c240f5d80706e7254abfc10391c3
HRVNoa <hervieu.noa@gmail.com>
2023-10-04 12:05:44 +0200

    Traitement du json via axios

--- src/components/ResultTab.vue
+++ src/components/ResultTab.vue
@@ -1,9 +1,43 @@
 <script setup>
+import axios from "axios";
+
+axios.get("http://127.0.0.1:9007/epreuve_athlete/64")
+    .then(response => {
+      // Gérer la réponse ici
+      const data = response.data;
+
+      const epreuveLength = Object.keys(data.epreuve).length;
+      const athleteLength = Object.keys(data.athlete).length;
+
+      console.log('Réponse de la requête :', data);
+
+      const tbodyresult = document.getElementById("tbody-result");
+
+      for (let i = 0; i < athleteLength; i++) {
+
+        console.log(data.athlete.nom);
+      }
+
+      const tr = "<tr>" +
+          "          <td>AGBEGNENOU</td>" +
+          "          <td>Clarisseo</td>" +
+          "          <td>31</td>" +
+          "          <td>FRANCE</td>" +
+          "        </tr>";
+
+      tbodyresult.innerHTML = tr;
+
+
+    })
+    .catch(error => {
+      // Gérer les erreurs ici
+      console.error('Erreur lors de la requête :', error);
+    });
 
 </script>
 
 <template>
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <div id="div-background-result-tab">
     <div id="div-result-tab">
       <table id="result-tab" class="table table-striped table-dark">
@@ -15,97 +49,8 @@
           <th>Pays</th>
         </tr>
         </thead>
-        <tbody>
-        <tr>
-          <td>AGBEGNENOU</td>
-          <td>Clarisseo</td>
-          <td>31</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
-        <tr>
-          <td>RINNER</td>
-          <td>Teddy</td>
-          <td>34</td>
-          <td>FRANCE</td>
-        </tr>
+        <tbody id="tbody-result">
+
         </tbody>
       </table>
     </div>
