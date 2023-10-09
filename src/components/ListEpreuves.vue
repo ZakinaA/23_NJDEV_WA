2b515d9f56f743196bb336fcc3501954718e1c55
HRVNoa <hervieu.noa@gmail.com>
2023-10-09 15:13:45 +0200

    Fusion faite sur les bouton resultat de chaque epreuve

--- src/components/ListEpreuves.vue
+++ src/components/ListEpreuves.vue
@@ -2,38 +2,115 @@
 
 import TabSportEpreuve from "@/components/tabSportEpreuve.vue";
 import axios from "axios";
+import ResultTab from "@/components/ResultTab.vue";
 
+function calculerAge(dateNaissance) {
+  const dateNaissanceObj = new Date(dateNaissance);
+  const dateActuelle = new Date();
 
-  axios.get("http://localhost:9007/epreuves").then(response => {
+  const anneeNaissance = dateNaissanceObj.getFullYear();
+  const moisNaissance = dateNaissanceObj.getMonth();
+  const jourNaissance = dateNaissanceObj.getDate();
 
-    //Gestion de la reponse
-    const data = response.data.sort((a, b) => a.id - b.id);
-    const divListAffichageEpreuve = document.getElementById("div-list-affichage-epreuve");
+  const anneeActuelle = dateActuelle.getFullYear();
+  const moisActuel = dateActuelle.getMonth();
+  const jourActuel = dateActuelle.getDate();
 
+  let age = anneeActuelle - anneeNaissance;
 
-    for (let i = 0; i < data.length; i++) {
-      const le = "<p>"+ data[i].libelle+"</p>" +
-          "<button class='btn-tabresult' id='button-resultat'>Resultat</button>";
-      divListAffichageEpreuve.innerHTML += le;
-      const buttonResultat = document.getElementById("button-resultat");
-      buttonResultat.id = data[i].id;
+  if (moisActuel < moisNaissance || (moisActuel === moisNaissance && jourActuel < jourNaissance)) {
+    age--;
+  }
 
+  return age;
+}
+function getTabResult(id) {
+  axios.get("http://127.0.0.1:9007/epreuve_athlete/"+id).then(response => {
 
+    const data = response.data.sort((a, b) => a.place - b.place);
+
+    const dataLength = Object.keys(data).length;
+
+    // console.log('Réponse de la requête :', data);
+
+    const tbodyresult = document.getElementById("tbody-result");
+    const divBackgroundResultTab = document.getElementById("div-background-result-tab");
+
+    let tr = "";
+
+    for (let i = 0; i < dataLength; i++) {
+      tr += "<tr>" +
+          "          <td>"+data[i].athlete.nom+"</td>" +
+          "          <td>"+data[i].athlete.prenom+"</td>" +
+          "          <td>"+calculerAge(data[i].athlete.date_naissance)+"</td>" +
+          "          <td>"+data[i].athlete.pays.libelle+"</td>" +
+          "          <td>"+data[i].place+"</td>" +
+          "        </tr>";
     }
-
+    divBackgroundResultTab.style.display = "flex";
+    tbodyresult.innerHTML = tr;
   })
+      .catch(error => {
+        // Gérer les erreurs ici
+        console.error('Erreur lors de la requête :', error);
+      });
+}
+
+axios.get("http://localhost:9007/epreuves").then(response => {
+
+  //Gestion de la reponse
+  const data = response.data.sort((a, b) => a.id - b.id);
+  const divListAffichageEpreuve = document.getElementById("div-list-affichage-epreuve");
 
 
+  for (let i = 0; i < data.length; i++) {
+    const le = "<div class=\"div-content-epreuve\">\n" +
+        "        <p>"+ data[i].libelle +"</p>\n" +
+        "        <p class='btnTabresult' id='buttonResultat'>Resultat</p>\n" +
+        "      </div>";
+    divListAffichageEpreuve.innerHTML += le;
+    const buttonResultat = document.getElementById("buttonResultat");
+    buttonResultat.id = data[i].id;
+    buttonResultat.style.color = "darkcyan";
+  }
+});
+
+setTimeout(function () {
+  const closeResult = document.getElementsByClassName("btnTabresult");
+  console.log(closeResult);
+  console.log(closeResult.length);
+  for (let i = 0; i < closeResult.length; i++) {
+
+    const id = closeResult[i].id;
+    const buttonResultat = document.getElementById(id);
+
+    buttonResultat.addEventListener("click", function () {
+      getTabResult(id);
+    });
+  }
+}, 500);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const closeResult = document.getElementById("closeResult");
+  closeResult.addEventListener("click", function () {
+    const divBackgroundResultTab = document.getElementById("div-background-result-tab");
+    divBackgroundResultTab.style.display = "none";
+  });
+});
 
 </script>
 
 <template>
+  <result-tab/>
 
   <div id="div-list-epreuve">
     <div id="div-list-affichage-epreuve">
 
+
+
     </div>
   </div>
+
 
 </template>
 
--- src/components/ResultTab.vue
+++ src/components/ResultTab.vue
@@ -1,85 +1,12 @@
 <script setup>
-import axios from "axios";
-
-function calculerAge(dateNaissance) {
-  const dateNaissanceObj = new Date(dateNaissance);
-  const dateActuelle = new Date();
-
-  const anneeNaissance = dateNaissanceObj.getFullYear();
-  const moisNaissance = dateNaissanceObj.getMonth();
-  const jourNaissance = dateNaissanceObj.getDate();
-
-  const anneeActuelle = dateActuelle.getFullYear();
-  const moisActuel = dateActuelle.getMonth();
-  const jourActuel = dateActuelle.getDate();
-
-  let age = anneeActuelle - anneeNaissance;
-
-  if (moisActuel < moisNaissance || (moisActuel === moisNaissance && jourActuel < jourNaissance)) {
-    age--;
-  }
-
-  return age;
-}
-
-function getTabResult(id) {
-  axios.get("http://127.0.0.1:9007/epreuve_athlete/"+id).then(response => {
-
-    const data = response.data.sort((a, b) => a.place - b.place);
-
-    const dataLength = Object.keys(data).length;
-
-    // console.log('Réponse de la requête :', data);
-
-    const tbodyresult = document.getElementById("tbody-result");
-    const divBackgroundResultTab = document.getElementById("div-background-result-tab");
-
-    let tr = "";
-
-    for (let i = 0; i < dataLength; i++) {
-      tr += "<tr>" +
-          "          <td>"+data[i].athlete.nom+"</td>" +
-          "          <td>"+data[i].athlete.prenom+"</td>" +
-          "          <td>"+calculerAge(data[i].athlete.date_naissance)+"</td>" +
-          "          <td>"+data[i].athlete.pays.libelle+"</td>" +
-          "          <td>"+data[i].place+"</td>" +
-          "        </tr>";
-    }
-    divBackgroundResultTab.style.display = "flex";
-    tbodyresult.innerHTML = tr;
-  })
-  .catch(error => {
-    // Gérer les erreurs ici
-    console.error('Erreur lors de la requête :', error);
-  });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const btnTabResult = document.getElementsByClassName("btn-tabresult");
-  for (let i = 0; i < btnTabResult.length; i++) {
-    btnTabResult[i].addEventListener("click", function () {
-      getTabResult(btnTabResult[i].id);
-    });
-  }
-
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const closeResult = document.getElementById("closeResult");
-  closeResult.addEventListener("click", function () {
-    const divBackgroundResultTab = document.getElementById("div-background-result-tab");
-    divBackgroundResultTab.style.display = "none";
-  });
-
-});
 
 </script>
 
 <template>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-  <p class="btn-tabresult" id="64">Test sur l'epreuve 64</p><br>
-  <p class="btn-tabresult" id="65">Test sur l'epreuve 65</p>
+<!--  <p class="btn-tabresult" id="64">Test sur l'epreuve 64</p><br>-->
+<!--  <p class="btn-tabresult" id="65">Test sur l'epreuve 65</p>-->
 
   <div id="div-background-result-tab">
     <h1 id="closeResult">x</h1>
