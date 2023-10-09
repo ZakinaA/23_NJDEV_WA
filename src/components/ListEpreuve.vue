2d80518324836330ffcd07497c1f5ea79a863463
adminsio <roussel.louane@gmail.com>
2023-10-09 11:43:33 +0200

    Lister les epreuves pour tous

--- src/components/ListEpreuve.vue
+++ src/components/ListEpreuve.vue
@@ -1,18 +1,31 @@
 <script setup>
 
 import TabSportEpreuve from "@/components/tabSportEpreuve.vue";
+import axios from "axios";
+
+
+  axios.get("http://localhost:9007/epreuves").then(response => {
+
+    //Gestion de la reponse
+    const data = response.data.sort((a, b) => a.id - b.id);
+    const divListEpreuve = document.getElementById("div-list-epreuve");
+
+    let le = "";
+
+    for (let i = 0; i < data.length; i++) {
+      le += "<p>"+ data[i].libelle+"</p>" +
+          "<button id='button-resultat-epreuve'>Resultat</button>"
+    }
+    divListEpreuve.innerHTML = le;
+  })
+
 </script>
 
 <template>
-<!--  <textarea id="recherche_epreuve">Recherche</textarea>-->
 
-<!--  <select name="olympiade" id="olympiade_select">-->
-<!--    <option value="">Olympiade</option>-->
-<!--    <option value=""></option>-->
-<!--  </select>-->
+  <div id="div-list-epreuve">
 
-  <tab-sport-epreuve/>
-
+  </div>
 
 </template>
 
